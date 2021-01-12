41d74c8d752af76a5cdb8165e319857e862739cf
jinwook.im <jin3670@gmail.com>
2021-01-12 22:31:44 +0900

    assertions generator 사용

---
+++ src/main/java/com/gintire/springboottest/domain/Player.java
@@ -0,0 +1,12 @@
+package com.gintire.springboottest.domain;/**
+ * Project: spring-boot-test
+ * Package: com.gintire.springboottest.domain
+ * <p>
+ * User: jin36
+ * Date: 2021-01-12
+ * Time: 오후 9:50
+ * <p>
+ * Created with IntelliJ IDEA
+ * To change this template use File | Settings | File Templates.
+ */public class Player {
+}
---
+++ src/test/java/com/gintire/springboottest/assertj/PlayerTest.java
@@ -0,0 +1,12 @@
+package com.gintire.springboottest.assertj;/**
+ * Project: spring-boot-test
+ * Package: com.gintire.springboottest.assertj
+ * <p>
+ * User: jin36
+ * Date: 2021-01-12
+ * Time: 오후 9:52
+ * <p>
+ * Created with IntelliJ IDEA
+ * To change this template use File | Settings | File Templates.
+ */public class PlayerTest {
+}
